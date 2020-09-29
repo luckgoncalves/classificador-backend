3e697259c12bbe8cdf81f784d2498075d2b57b89
Lucas Goncalves <lucasgoncalves@Lucass-MacBook-Pro.local>
2020-09-28 21:07:26 -0300

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const requireDir = require('require-dir');
 //Iniciando App
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 //Iniciando o DB
